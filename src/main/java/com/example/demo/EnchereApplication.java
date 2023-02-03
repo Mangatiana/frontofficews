3adb4a4ad5bdf1180a053275fc9c4e70592103c4
Mangatiana <99736239+Mangatiana@users.noreply.github.com>
2023-02-03 22:05:56 +0300

    Update EnchereApplication.java

--- src/main/java/com/example/demo/EnchereApplication.java
+++ src/main/java/com/example/demo/EnchereApplication.java
@@ -3,8 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.coonfig.annotation.CorsRegistry;
-import org.springframework.web.servlet.coonfig.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
