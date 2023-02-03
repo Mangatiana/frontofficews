b1d828996d6ad4963e46a75019d916e49a11de08
Mangatiana <99736239+Mangatiana@users.noreply.github.com>
2023-02-03 22:02:51 +0300

    Update EnchereApplication.java

--- src/main/java/com/example/demo/EnchereApplication.java
+++ src/main/java/com/example/demo/EnchereApplication.java
@@ -2,14 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.coonfig.annotation.CorsRegistry;
+import org.springframework.web.servlet.coonfig.annotation.WebMvcConfigurer;
 
-@RestController
-@CrossOrigin
+
 @SpringBootApplication
 public class EnchereApplication {
-    /*@Bean
+    @Bean
     public WebMvcConfigurer corsConfigurer(){
         return new WebMvcConfigurer() {
             @Override
@@ -18,7 +18,7 @@ public class EnchereApplication {
                 registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedOrigins("*").allowedMethods("*").allowedHeaders("*").maxAge(-1).allowCredentials(false);
             }
         };
-    } */
+    } 
     public static void main(String[] args) {
         SpringApplication.run(EnchereApplication.class, args);
     }
